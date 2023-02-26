8e4f50403e4779aa0601935af6cc2073b7e0aadb
Mark Andrey <markandreydelacruz@ymail.com>
2023-02-26 16:41:50 +0800

    rename app to "Weather App One"

--- ios/RN_AwesomeProjectTests/RN_AwesomeProjectTests.m
+++ ios/RN_AwesomeProjectTests/RN_AwesomeProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RN_AwesomeProjectTests : XCTestCase
+@interface WeatherAppManilaTests : XCTestCase
 
 @end
 
-@implementation RN_AwesomeProjectTests
+@implementation WeatherAppManilaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
